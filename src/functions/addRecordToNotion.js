7a08e0bbcc9354bc90db5dd12a39e89b485b125a
Bruno Hubner <brunohubnerdev@gmail.com>
2023-12-27 21:17:38 -0300

    feat: add retry functionality for creating Notion page

--- src/functions/addRecordToNotion.js
+++ src/functions/addRecordToNotion.js
@@ -1,11 +1,12 @@
 const env = require('../config/env');
 const notion = require('../config/notion');
+const retryPromise = require('./retryPromise');
 
 module.exports = async function addRecordToNotion(properties) {
-  await notion.pages.create({
+  await retryPromise(() => notion.pages.create({
     parent: {
       database_id: env.NOTION_DATABASE_ID,
     },
     properties,
-  });
+  }));
 };
