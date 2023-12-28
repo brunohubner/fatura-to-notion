3329bfeac16f94ff3a9149e0f66e1b99ffcb6b15
Bruno Hubner <brunohubnerdev@gmail.com>
2023-12-27 21:14:19 -0300

    feat: add retryPromise function

---
+++ src/functions/retryPromise.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+module.exports = async function retryPromise(
+  promise,
+  retriesLeft = 5,
+) {
+  try {
+    const result = await promise();
+    return result;
+  } catch (error) {
+    console.error(`ERROR: ${error.message || JSON.stringify(error, null, 2)};`);
+
+    if (retriesLeft > 0) {
+      console.warn(`Tentando resolver Promise novamente, ${retriesLeft} tentativas restantes;`);
+      return retryPromise(promise, retriesLeft - 1);
+    }
+
+    throw new Error(error);
+  }
+};
